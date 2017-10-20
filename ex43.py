954d2d68837a2b17e25a5ee7f3a713ca5dbd7e35
lincky <linckypast@hotmail.com>
2017-10-20 20:58:50 +0800

    Revert "Revert "ex43:Map&Engine ProtoType""
    This reverts commit 441068b2ede4ad382b4af1df303161dd03f094b9.

---
+++ ex43.py
@@ -0,0 +1,35 @@
+class Map(object):
+    
+    scenes = {
+        'central_corridor': CentralCorridor(),
+        'laser_weapon_armory': LaserWeaponArmory(),
+        'the_bridge': TheBridge(),
+        'escape_pod': EscapePod(),
+        'death': Death(),
+        }
+
+    def __init__(self, start_scene):
+        self.start_scene = start_scene
+
+    def next_scene(self, scene_name):
+        return Map.scenes.get(scene_name)
+
+    def opening_scene(self):
+        return self.next_scene(self.start_scene)
+
+class Engine(object):
+
+    def __init__(self, scene_map):
+        self.scene_map = scene_map
+
+    def play(self):
+        current_scene = self.scene_map.opening_scene()
+
+        while True:
+            print "\n--------"
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.next_scene(next_scene_name)
+
+a_map = Map('central_corridor')
+a_game = Engine(a_map)
+a_game.play()
